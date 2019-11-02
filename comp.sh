0ab4018d6ef5784ccba603d74b9112e0bea31fae
VexyChan <48259355+VexyChan@users.noreply.github.com>
2019-11-02 17:40:38 -0400

    Create comp.sh

---
+++ comp.sh
@@ -0,0 +1,24 @@
+nasm BootLoader.asm
+dd if=/dev/zero of=diskc.img bs=512 count=1000
+dd if=BootLoader of=diskc.img bs=512 count=1 conv=notrunc
+bcc -ansi -c -o kernel_c.o kernel.c
+as86 kernal.asm -o kernel_asm.o
+ld86 -o kernel -d kernel_c.o kernel_asm.o
+./loadFile kernel
+./loadFile message.txt
+bcc -ansi -c -o tstpr1.o tstpr1.c
+as86 -o userlib.o userlib.asm
+ld86 -d -o tstpr1 tstpr1.o userlib.o
+bcc -ansi -c -o tstpr2.o tstpr2.c
+as86 -o userlib.o userlib.asm
+ld86 -d -o tstpr2 tstpr2.o userlib.o
+./loadFile tstpr1
+./loadFile tstpr2 
+bcc -ansi -c -o shell.o Shell.c
+as86 userlib.asm -o shell_asm.o
+ld86 -o shell -d shell.o Shell_asm.o
+./loadFile shell
+bcc -ansi -c -o tst.o tst.c
+as86 -o userlib.o userlib.asm
+ld86 -d -o tst tst.o userlib.o
+./loadFile tst
