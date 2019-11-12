dafb3154b9b00c61f0421f7ee8afc8253d3d6bcb
VexyChan <48259355+VexyChan@users.noreply.github.com>
2019-11-12 15:33:31 -0500

    Add files via upload

--- Shell.c
+++ Shell.c
@@ -1,12 +1,24 @@
 main(){
 	char line[80];
+	char filen[8];
+	char buff[13312];
 	char* command[4];
 	int index=0;
 	command[0]="help";
 	command[1]="type";
 	command[2]="exec";
+	command[3]="files";
+	command[4]='\0';
 	syscall(0,"SHELL>",0);
 	syscall(1,line,0);
+	filen[0]='\0';
+	filen[1]='\0';
+	filen[2]='\0';
+	filen[3]='\0';
+	filen[4]='\0';
+	filen[5]='\0';
+	filen[6]='\0';
+
 	while(command[index]!='\0'){
 		int l=0;
 		int trufal =1;		
@@ -26,26 +38,46 @@ main(){
 				while(command[g]!='\0'){
 					syscall(0,command[g],0);
 					syscall(0," ",0);
-					g++;
-									
+					g++;			
 				}
 				syscall(0,"\r\n",0);
-				break;	
+				syscall(5,0,0);			
 			}
 			else if(index==1){
-				syscall(0,"I PRINT TYPE\r\n",0);
-				break;
+				//type 01234[5]
+				filen[0]=line[5];
+				filen[1]=line[6];
+				filen[2]=line[7];
+				filen[3]=line[8];
+				filen[4]=line[9];
+				filen[5]=line[10];
+				syscall(3,filen,buff);
+				syscall(0,buff,0);
+				syscall(0,"\r\n",0);
+				syscall(5,0,0);
 			}
 			else if(index==2){
-				syscall(0,"I exec\r\n",0);
-				break;
+				filen[0]=line[5];
+				filen[1]=line[6];
+				filen[2]=line[7];
+				filen[3]=line[8];
+				filen[4]=line[9];
+				filen[5]=line[10];
+				syscall(4,filen,0);			
+				syscall(5,0,0);
 			}
-			else if(index== 3){
+			else if (index==3){
+				syscall(7,0,0);
+				syscall(0,"\r\n",0);
+				syscall(5,0,0);		
+			}
+			else if(index== 4){
 				syscall(0,"Command Not Found\r\n",0);
-				break;
-			}
+				syscall(5,0,0);
+	
+			}	
+			
 		}
-
 		index++;
 	}					
 	syscall(5,0,0);
